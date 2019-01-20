3d0731d34accc154afdf720d91ff3313f79ddcd7
Roman Khvostikov <romankhvostikov@gmail.com>
2019-01-21 00:25:43 +0300

    First draft

--- CPPDbgVis_test.cpp
+++ CPPDbgVis_test.cpp
@@ -1,21 +1,48 @@
-// CPPDbgVis_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 
-int main()
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/signals2.hpp>
+
+void __fake_code_line()
+{}
+
+void test_SmartPointers()
 {
-    std::cout << "Hello World!\n"; 
+	boost::shared_ptr<int> sh;
+	sh.reset( new int( 5 ) );
+
+	boost::weak_ptr<int> w;
+	w = sh;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void test_ptree()
+{
+	boost::property_tree::wptree t;
+//	t.add_child( L"item1", 1 );
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void test_signals()
+{
+	boost::signals2::scoped_connection c;
+	{
+		boost::signals2::signal<void( bool )> s;
+		c = s.connect( []( bool ) {} );
+		{
+			boost::signals2::scoped_connection c2;
+			c2 = s.connect( []( bool ) {} );
+			__fake_code_line();
+		}
+		__fake_code_line();
+	}
+	__fake_code_line();
+}
+
+int main()
+{
+	test_SmartPointers();
+	test_ptree();
+	test_signals();
+}
