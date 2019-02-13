69dc17bd9bb2fc57cd1da75d26a3e305a32cc55c
Roman Khvostikov <roman_khvostikov@mentor.com>
2019-02-13 18:10:30 +0300

    project adapted to v140

--- CPPDbgVis_test.cpp
+++ CPPDbgVis_test.cpp
@@ -5,24 +5,60 @@
 #include <boost/weak_ptr.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/signals2.hpp>
+#include <boost/variant.hpp>
+
+//////////////////////////////////////////////////////////////////////////
+
+class AClass
+{
+public:
+	void foo()
+	{}
+};
 
 void __fake_code_line()
 {}
+
+//////////////////////////////////////////////////////////////////////////
+
+void test_function()
+{
+	boost::function<void(void)> f;
+	f = [](){};
+	__fake_code_line();
+}
+
+//////////////////////////////////////////////////////////////////////////
 
 void test_SmartPointers()
 {
 	boost::shared_ptr<int> sh;
 	sh.reset( new int( 5 ) );
 
+	boost::shared_ptr<AClass> sh2( new AClass );
+	sh2->foo();
+
 	boost::weak_ptr<int> w;
 	w = sh;
 }
 
+//////////////////////////////////////////////////////////////////////////
+
+void test_optional()
+{}
+
+//////////////////////////////////////////////////////////////////////////
+
 void test_ptree()
 {
-	boost::property_tree::wptree t;
-//	t.add_child( L"item1", 1 );
+	boost::property_tree::wptree t, t2;
+	t.put_value( 1.1 );
+	t.put_value( L"string value" );
+	t2.put_child( L"child1", t );
+	__fake_code_line();
 }
+
+//////////////////////////////////////////////////////////////////////////
 
 void test_signals()
 {
@@ -40,9 +76,29 @@ void test_signals()
 	__fake_code_line();
 }
 
+//////////////////////////////////////////////////////////////////////////
+
+void test_variant()
+{
+	boost::variant<int,bool> v2;
+	v2 = 1;
+	_ASSERTE( boost::get<int>(v2) == 1 );
+	v2 = true;
+	_ASSERTE( boost::get<bool>(v2) == true );
+
+	boost::variant<int,bool,std::wstring> v3;
+	v3 = 1;
+	v3 = std::wstring(L"string");
+}
+
+//////////////////////////////////////////////////////////////////////////
+
 int main()
 {
-	test_SmartPointers();
+	test_function();
+	test_optional();
 	test_ptree();
 	test_signals();
+	test_SmartPointers();
+	test_variant();
 }
